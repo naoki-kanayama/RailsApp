04b0de2a2c2ef8a1577c914c421f31b44f4ede28
naoki-kanayama <89069264+naoki-kanayama@users.noreply.github.com>
2021-11-02 15:08:47 +0900

    initial commit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,21 @@
+class HelloController < ApplicationController
+
+  def index
+    if params['msg'] != nil then
+      msg = 'Hello, ' + params['msg'] + '!'
+    else
+      msg = 'this is Rails smaple page.'
+    end
+
+    html = '
+    <html>
+    <body>
+      <h1>Sample Page</h1>
+      <p>' + msg + '</p>
+    </body>
+    </html>
+    '
+    render html: html.html_safe
+  end
+
+end
